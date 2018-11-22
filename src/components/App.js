6283bb69374db6f162793977e6112687c52fa7e9
ipek <ipekcivicakan@gmail.com>
2018-11-22 11:07:04 -0800

    boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+const App = () => {
+    return (
+    <div>
+        <SearchBar className="ui conteiner" style={{marginTop: '10px'}}/>
+    </div>
+    )
+};
+
+export default App;
