1e5cc98e0e5c5be9d1cd8446d699583f756d801a
ipek <ipekcivicakan@gmail.com>
2018-11-22 11:29:27 -0800

    Created event handler and alternate event handler syntax

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,18 @@
 import React,{Component} from 'react';
 
 class SearchBar extends Component {
+onChangeHandler(event) {
+    console.log(event.target.value)
+}
+
     render(){
         return(
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Image Research</label>
-                        <input type="text" />
+                        <input type="text" onChange={this.onChangeHandler} />
+                        {/*Alternate event handler syntax  <input type="text" onChange={(e) => e.target.value} /> */}
                     </div>
                 </form>
             </div>
