094268b4704e380fd8659896bb4e3d55657fc8d9
ipek <ipekcivicakan@gmail.com>
2018-11-22 16:56:59 -0800

    handled form submittal

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,13 +3,23 @@ import React,{Component} from 'react';
 class SearchBar extends Component {
 state={ term: ''}
 
+onSubmitHandler=(event)=>{
+
+    event.preventDefault();
+    console.log(this.state.term)
+
+}
+
     render(){
         return(
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onSubmitHandler} className="ui form">
                     <div className="field">
                         <label>Image Research</label>
-                        <input type="text" value={this.state.term} onChange={(e) => this.setState({term:e.target.value})} />
+                        <input 
+                        type="text" 
+                        value={this.state.term} 
+                        onChange={(e) => this.setState({term:e.target.value})} />
                         {/*<input type="text" onChange={this.onChangeHandler} /> Uncontrolled component - Event Handler*/}
                         {/*Alternate event handler syntax  <input type="text" onChange={(e) => e.target.value} /> */}
                     </div>
