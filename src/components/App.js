4339870e0c599d28341a1ed089bfa9533789113b
ipek <ipekcivicakan@gmail.com>
2018-11-23 17:13:50 -0800

    fetched data from unsplash

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component{
-
     submitHandler(term){
-        console.log(term)
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: {query: term},
+            headers: {
+                Authorization: 'Client-ID 89577192b5cc2901ce5634382e9ca92731545a54513deda75f1f634e1a80fe83'
+            }
+        })
+        .then(res => {
+            console.log(res.data.results)
+        });
+            
     }
 
     render(){
         return (
             <div>
-                <SearchBar submit={this.submitHandler}className="ui conteiner" style={{marginTop: '10px'}}/>
+                <SearchBar submit={this.submitHandler} className="ui conteiner" style={{marginTop: '10px'}}/>
             </div>
             );
     }
