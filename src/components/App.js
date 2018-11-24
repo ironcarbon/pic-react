62ff01d10a974ee36af30358ac545a8287996bc4
ipek <ipekcivicakan@gmail.com>
2018-11-24 09:59:21 -0800

    handling requests with async await

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,20 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component{
-    submitHandler(term){
-        axios.get('https://api.unsplash.com/search/photos', {
+
+    state={
+        images: []
+    }
+
+    submitHandler = async (term) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: {query: term},
             headers: {
                 Authorization: 'Client-ID 89577192b5cc2901ce5634382e9ca92731545a54513deda75f1f634e1a80fe83'
             }
-        })
-        .then(res => {
-            console.log(res.data.results)
         });
+        this.setState({images: response.data.results});
+        
             
     }
 
@@ -20,6 +24,7 @@ class App extends React.Component{
         return (
             <div>
                 <SearchBar submit={this.submitHandler} className="ui conteiner" style={{marginTop: '10px'}}/>
+                Found: {this.state.images.length} images
             </div>
             );
     }
