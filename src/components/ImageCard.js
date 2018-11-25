301dfb0bc192052a0939be094eaa9fbd3d5469f8
ipek <ipekcivicakan@gmail.com>
2018-11-24 22:35:29 -0800

    Using ref's for DOM access

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React from "react";
 
-class ImageCards extends React.Component{
-    render(){
-        const {urls, description} = this.props.image;
-        return(
-            <div>
-                <img 
-                src={urls.regular}
-                alt={description}
-                />
-            </div>
-        );
-    }
+class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.imageRef = React.createRef();
+    this.state = {
+      spans: 0
+    };
+  }
+
+  componentDidMount() {
+    // console.log(this.imageRef);
+    //console.log(this.imageRef.current.clientHeight);
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    //console.log(this.imageRef.current.clientHeight);
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10);
+    this.setState({ spans });
+  };
+
+  render() {
+    const { urls, description } = this.props.image;
+    return (
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} src={urls.regular} alt={description} />
+      </div>
+    );
+  }
 }
 
-export default ImageCards;
+export default ImageCard;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,31 +1,31 @@
-import React,{Component} from 'react';
+import React, { Component } from "react";
 
 class SearchBar extends Component {
-state={ term: ''}
+  state = { term: "" };
 
-onSubmitHandler=(event)=>{
-    this.props.submit(this.state.term)
+  onSubmitHandler = event => {
+    this.props.submit(this.state.term);
     event.preventDefault();
+  };
 
-}
-
-    render(){
-        return(
-            <div className="ui segment">
-                <form onSubmit={this.onSubmitHandler} className="ui form">
-                    <div className="field">
-                        <label>Image Research</label>
-                        <input 
-                        type="text" 
-                        value={this.state.term} 
-                        onChange={(e) => this.setState({term:e.target.value})} />
-                        {/*<input type="text" onChange={this.onChangeHandler} /> Uncontrolled component - Event Handler*/}
-                        {/*Alternate event handler syntax  <input type="text" onChange={(e) => e.target.value} /> */}
-                    </div>
-                </form>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onSubmitHandler} className="ui form">
+          <div className="field">
+            <label>Image Research</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
+            {/*<input type="text" onChange={this.onChangeHandler} /> Uncontrolled component - Event Handler*/}
+            {/*Alternate event handler syntax  <input type="text" onChange={(e) => e.target.value} /> */}
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
